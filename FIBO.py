3b33d3617a312d111652322304c6f6bcafffe800
varis vipavanich <63534193+varis22521@users.noreply.github.com>
2022-10-04 17:44:10 +0700

    Create FIBO.py

---
+++ FIBO.py
@@ -0,0 +1,51 @@
+import time 
+from colorama import Fore, Back, Style
+from progress.bar import IncrementalBar
+mylist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+bar = IncrementalBar('T MINUS', max = len(mylist))
+for item in mylist:
+    bar.next()
+    time.sleep(.1)
+bar.finish()
+periot = True
+while(periot):	
+	
+	print(Fore.BLUE + 'HI I AM VARIS')
+	time.sleep(3)
+	print(Fore.YELLOW + 'LES"T ME INTRUDUCE MYSELF')
+	time.sleep(3)
+	print("███████╗██╗██████╗░░█████╗░  ██╗░░██╗███╗░░░███╗██╗░░░██╗████████╗████████╗")
+	print("██╔════╝██║██╔══██╗██╔══██╗  ██║░██╔╝████╗░████║██║░░░██║╚══██╔══╝╚══██╔══╝")
+	print("█████╗░░██║██████╦╝██║░░██║  █████═╝░██╔████╔██║██║░░░██║░░░██║░░░░░░██║░░░")
+	print("██╔══╝░░██║██╔══██╗██║░░██║  ██╔═██╗░██║╚██╔╝██║██║░░░██║░░░██║░░░░░░██║░░░")
+	print("██║░░░░░██║██████╦╝╚█████╔╝  ██║░╚██╗██║░╚═╝░██║╚██████╔╝░░░██║░░░░░░██║░░░")
+	print("╚═╝░░░░░╚═╝╚═════╝░░╚════╝░  ╚═╝░░╚═╝╚═╝░░░░░╚═╝░╚═════╝░░░░╚═╝░░░░░░╚═╝░░░")
+	time.sleep(2)
+	print("██████╗ ██████╗  ██████╗ ███████╗██╗██╗     ███████╗")
+	print("██╔══██╗██╔══██╗██╔═══██╗██╔════╝██║██║     ██╔════╝")
+	print("██████╔╝██████╔╝██║   ██║█████╗  ██║██║     █████╗  ")
+	print("██╔═══╝ ██╔══██╗██║   ██║██╔══╝  ██║██║     ██╔══╝  ")
+	print("██║     ██║  ██║╚██████╔╝██║     ██║███████╗███████╗")
+	print("╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝╚══════╝")
+                                                    
+	time.sleep(2)
+	print(Fore.GREEN +"NAME: VARIS VIPAVANICH","   ","NICKNAME: franc")
+	print(Fore.GREEN +"ชื่อ: วาริส วิภาวนิช","           ","ชื่อเล่น: ฟรังก์")
+	time.sleep(1)
+	print("AGE: 17","		","Father's name ATSADET VIPAVANICH")
+	print("อายุ: 17","		","Mother's name PHATCHARIN VIPAVANICH")
+	time.sleep(1)
+	print("KINE OF BLOOD: A")
+	print("หมู่โลหิต: A")
+	time.sleep(1)
+	print("nationality: THAI")
+	print("สัญชาติ: ไทย")
+	time.sleep(1)
+	print("ethnicity: THAI")
+	print("เชื้อชาติ: ไทย")
+	time.sleep(1)
+	
+	periot = False
+	
+	
+	
